a8c517668c2cc97c571ed38d823a9d5be3154394
David Kremenitzer <dkrem21@gmail.com>
2021-12-04 14:43:44 -0500

    updated Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
               className="cta-btn cta-btn--resume"
               href={email ? `mailto:${email}` : 'https://www.linkedin.com/in/dkremenitzer/'}
             >
-              {btn || "Let's Connect!"}
+              {btn || "Let's Connectq"}
             </a>
           </div>
         </Fade>
